fd6816228ec9b1719e1517c51d163ffde9f79ca6
17luba <146106312+17luba@users.noreply.github.com>
2025-03-26 13:35:00 +0100

    Création de la balle et le script qui permet de detruire un objet quand il sera en contact de la balle

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float life = 3;
+
+    private void Awake()
+    {
+        Destroy(gameObject, life);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
